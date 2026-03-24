8c515f6456773b0d005635d7c574e53896de17ca
JamesDev209 <james1913_@hotmail.com>
2026-03-24 18:29:29 -0500

    feat: Loading ruta a el home

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,7 +1,21 @@
+import { Loader2Icon } from "lucide-react"
+import { useEffect } from "react"
 
 
 export const Loading = () => {
+
+  useEffect(() =>{
+    setTimeout(()=>{
+      window.location.href = '/'
+    }, 6000)
+  })
+
+
   return (
-    <div>Loading</div>
+    <div className="h-screen felx flex-col"> 
+        <div className="flex items-center justify-center fñex-1">
+            <Loader2Icon className="size-7 text-indigo-200 animate-spin"/>
+        </div>
+    </div>
   )
 }
